cbcd95d6dbf12aaec3e081983fe3205b0bc3d919
jingliancui <ericcui@qq.com>
2021-05-10 22:39:05 +0800

    Add sample code.

---
+++ README.md
@@ -0,0 +1,14 @@
+# XamarinFormsFSCalendarSample
+Sample for showing sFSCalendar features on Xamarin.Forms.
+
+Tutorial:
+
+
+
+QQ Group:313308215
+
+WeChat Official Account(微信公众号):
+
+Xamarin Library
+
+<img src="https://raw.githubusercontent.com/jingliancui/XamarinFormsMicrosoftAppCenterSample/master/Images/wechatqrcode.jpg"/>
---
+++ SampleApp/SampleApp.iOS/Renderers/XCalendarViewRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SampleApp.Controls;
+using Xamarin.Forms.Platform.iOS;
+using FSCalendar_API;
+using Xamarin.Forms;
+using SampleApp.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(XCalendarView), typeof(XCalendarViewRenderer))]
+namespace SampleApp.iOS.Renderers
+{
+    public class XCalendarViewRenderer:ViewRenderer<XCalendarView, FSCalendar>
+    {
+        private FSCalendar fSCalendar;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<XCalendarView> e)
+        {
+            fSCalendar = new FSCalendar
+            {
+                Frame = new CoreGraphics.CGRect(0,0,320,300),
+                DataSource=new FSCalendarDataSource(),
+                Delegate=new FSCalendarDelegate()
+            };
+            SetNativeControl(fSCalendar);
+        }
+    }
+}
